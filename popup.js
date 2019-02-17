ae07b47ebcae137d1ccbd694d90433b7f310c7ed
Maris <maris.kori@gmail.com>
2019-02-17 23:24:34 +0300

    0.3

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ let background = chrome.extension.getBackgroundPage();
 
 document.addEventListener('DOMContentLoaded', function () {
 	if (background.localStorage.db) {
-		const len = background.localStorage.db.length + (localStorage.getItem('formsData') || '').length;
+		const len = background.localStorage.db.length + (background.localStorage.getItem('formsData') || '').length;
 		const perc = Math.round(len / 5000000 * 1000)/10;
 		document.body.innerHTML = 'Кэш занимает: '+perc+'%';
 	}
